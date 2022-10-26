e18b8ffaddd6e5ff58f02ecf2c6a0b0a26ced67d
AlanVitor <alanvictor100@hotmail.com>
2022-10-26 00:47:27 -0300

    subindo static

--- Django2/settings.py
+++ Django2/settings.py
@@ -121,6 +121,8 @@ STATIC_URL = '/static/'# usado durante o desenvolvimento
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
